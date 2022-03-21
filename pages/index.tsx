6a15626d30b4b187ab3e04718232e6e7d2e93e98
Luis <melendez12341@gmail.com>
2022-03-21 18:57:17 -0400

    fix: formulario no estaba centrado

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
 
     return (
         <Layout>
-            <div className='lg:min-w-max max-w-7xl flex justify-center items-center pt-10'>
+            <div className='w-full flex justify-center items-center pt-10'>
                 <ShipmentForm/>
             </div>
         </Layout>
